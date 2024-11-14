bb79dd310002a48c3d59bf1e3f368a6a0701c66b
ShiwaKathariya <shiwa.kathariya@routecue.com,au>
2024-11-14 14:40:14 +0545

    Spa Service added

--- Code/DahitanGhar/Program.cs
+++ Code/DahitanGhar/Program.cs
@@ -5,15 +5,26 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+//Config Angular UI
+builder.Services.AddSpaStaticFiles(configuration =>
+{
+    configuration.RootPath = "Client/dist/Client/browser";
+});
 
 var app = builder.Build();
 
+var env = app.Environment;
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (env.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    // Use custom exception handler middleware early in the pipeline
+    app.UseExceptionHandler("/Home/error"); // Custom path or middleware
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -21,5 +32,15 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseSpa(spa =>
+{
+    spa.Options.SourcePath = "Client/dist/Client/browser"; //this will be used for production...
+
+    if (env.IsDevelopment())
+    {
+        spa.UseProxyToSpaDevelopmentServer(builder.Configuration["SpaBaseUrl"] ?? "http://localhost:4200");
+    }
+});
 
 app.Run();
