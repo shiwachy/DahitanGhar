20e2d347ed84e277612bc2a6ec89ebac5978a84b
ShiwaKathariya <shiwa.kathariya@routecue.com,au>
2025-09-02 22:10:57 +0545

    reset application

--- Code/DahitanGhar/Client/src/app/app.component.ts
+++ Code/DahitanGhar/Client/src/app/app.component.ts
@@ -1,40 +1,12 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { ButtonModule } from 'primeng/button';
-import { NavBarComponent } from "./nav-bar/nav-bar.component";
-import { AvatarModule } from 'primeng/avatar';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { DialogModule } from 'primeng/dialog';
-import { LoginComponent } from "./auth/login/login.component";
-import { AuthClient } from './dgApiClient';
-import { HttpClient } from '@angular/common/http';
-import { AuthService } from './shared/auth.service';
+import { Component } from "@angular/core";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonModule, NavBarComponent,
-    AvatarModule,
-    CommonModule,
-    FormsModule,
-    DialogModule, LoginComponent],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  providers: [AuthClient, AuthService]
+  providers: []
 })
 export class AppComponent {
-  public isMenubarVisible: boolean = false;
-  isLoginPageVisible: boolean = false;
 
-  ToggleMenuBar() {
-    this.isMenubarVisible = !this.isMenubarVisible;
-  }
-
-  OnClickBodyPage() {
-    this.isMenubarVisible = false;
-  }
-
-  ShowLoginPage() {
-    this.isLoginPageVisible = true;
-  }
 }
--- Code/DahitanGhar/Client/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomePageComponent } from './home-page/home-page.component';
-
-export const routes: Routes = [
-    { path: '', component: HomePageComponent },
-    { path: 'home-page', component: HomePageComponent }
-];
