7d0e6f43efb4411c8b6a1c9ce1878ad56aee6deb
ShiwaKathariya <shiwa.kathariya@routecue.com,au>
2025-09-03 00:14:23 +0545

    #primeng and bootstrap ui library installed.

--- Code/DahitanGhar/Client/src/app/app.component.ts
+++ Code/DahitanGhar/Client/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: []
 })
 export class AppComponent {
   title = 'DahitanGhar';
+
+  visible: boolean = false;
 }
