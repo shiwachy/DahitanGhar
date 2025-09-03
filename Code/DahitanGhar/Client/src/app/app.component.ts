4cbe31122fac24684595aecd57b2daf1d454e7d1
ShiwaKathariya <shiwa.kathariya@routecue.com,au>
2025-09-03 10:17:03 +0545

    Initial setup: Fonts,Sacings configured.

--- Code/DahitanGhar/Client/src/app/app.component.ts
+++ Code/DahitanGhar/Client/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "./header/header.component";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: []
+  imports: [HeaderComponent]
 })
 export class AppComponent {
-  title = 'DahitanGhar';
 
-  visible: boolean = false;
 }
