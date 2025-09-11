10f471c0aa5d9d3b7563d92e11fd8eeed33a0bca
ShiwaKathariya <shiwa.kathariya@routecue.com,au>
2025-09-11 22:33:49 +0545

    Service-card section created.

--- Code/DahitanGhar/Client/src/app/home/home.component.ts
+++ Code/DahitanGhar/Client/src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { CarouselModule } from 'primeng/carousel';
+import { ServiceCardComponent } from "./service-card/service-card.component";
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
-  imports: [CarouselModule]
+  imports: [CarouselModule, ServiceCardComponent]
 })
 export class HomeComponent implements OnInit {
   products: string[] = ["img1.jpg", "img2.jpg", "img3.jpg", "img4.jpg"];
