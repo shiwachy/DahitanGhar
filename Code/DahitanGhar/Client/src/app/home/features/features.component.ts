1763369ea582e50c48e37d3ab06b023d5eccdd2f
ShiwaKathariya <shiwa.kathariya@routecue.com,au>
2025-09-12 00:08:11 +0545

    Feature section structure created.

---
+++ Code/DahitanGhar/Client/src/app/home/features/features.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'features',
+  templateUrl: './features.component.html',
+  styleUrl: './features.component.css',
+  imports: []
+})
+export class FeaturesComponent {
+
+}
--- Code/DahitanGhar/Client/src/app/home/home.component.ts
+++ Code/DahitanGhar/Client/src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { CarouselModule } from 'primeng/carousel';
+import { FeaturesComponent } from "./features/features.component";
 import { ServiceCardComponent } from "./service-card/service-card.component";
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
-  imports: [CarouselModule, ServiceCardComponent]
+  imports: [CarouselModule, ServiceCardComponent, FeaturesComponent]
 })
 export class HomeComponent implements OnInit {
   products: string[] = ["img1.jpg", "img2.jpg", "img3.jpg", "img4.jpg"];
