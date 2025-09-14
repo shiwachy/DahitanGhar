b59826f490f05ae4fab79b0ff24d0da62f0feb42
ShiwaKathariya <shiwa.kathariya@routecue.com,au>
2025-09-14 18:10:08 +0545

    Feature section designed.

---
+++ Code/DahitanGhar/Client/public/shared/shared-enum.ts
@@ -0,0 +1,6 @@
+
+//Used to define position of visual and text section in a component.
+export enum Enum_GridTemplateStyle {
+    ltr = "ltr",
+    rtl = "rtl"
+}
--- Code/DahitanGhar/Client/src/app/home/home.component.ts
+++ Code/DahitanGhar/Client/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CarouselModule } from 'primeng/carousel';
+import { Enum_GridTemplateStyle } from '../../../public/shared/shared-enum';
 import { FeaturesComponent } from "./features/features.component";
 import { ServiceCardComponent } from "./service-card/service-card.component";
 @Component({
@@ -8,12 +9,10 @@ import { ServiceCardComponent } from "./service-card/service-card.component";
   styleUrl: './home.component.css',
   imports: [CarouselModule, ServiceCardComponent, FeaturesComponent]
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
   products: string[] = ["img1.jpg", "img2.jpg", "img3.jpg", "img4.jpg"];
 
   responsiveOptions: any[] | undefined;
 
-  ngOnInit(): void {
-
-  }
+  public _enum_GridTemplateStyle = Enum_GridTemplateStyle;
 }
