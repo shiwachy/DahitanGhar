8531ff54417d42dcd78c45f62a225c3e832648d0
ShiwaKathariya <shiwa.kathariya@routecue.com,au>
2025-09-16 20:55:11 +0545

    Reservation and Footer section

---
+++ Code/DahitanGhar/Client/src/app/footer/footer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'footer-section',
+  imports: [],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css'
+})
+export class FooterComponent {
+
+}
